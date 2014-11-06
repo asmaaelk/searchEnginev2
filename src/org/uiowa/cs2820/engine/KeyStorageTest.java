dba52df102e7f2e6ecf2437f6135be8b4419b02f
MidnightPolaris <MidnightPolaris@users.noreply.github.com>
2014-11-06 15:05:48 -0600

    Updated tests

--- src/org/uiowa/cs2820/engine/KeyStorageTest.java
+++ src/org/uiowa/cs2820/engine/KeyStorageTest.java
@@ -18,7 +18,7 @@ public class KeyStorageTest {
 	@Before
 	public void before(){
 		ks = new KeyStorage();
-		assertEquals(0,ks.size());
+		assertEquals(0,ks.getKeys().size());
 	}
 	
 	@After
@@ -47,7 +47,7 @@ public class KeyStorageTest {
 		ks.add(f1);
 		assertNotNull(ks.find(f1));
 		assertNull(ks.find(f2));
-		assertEquals(1,ks.size());
+		assertEquals(1,ks.getKeys().size());
 	}
 	
 	@Test
@@ -55,7 +55,7 @@ public class KeyStorageTest {
 		ks.add(f1);
 		assertNotNull(ks.key(f1));
 		assertNotNull(ks.key(f2));
-		assertEquals(2,ks.size());
+		assertEquals(2,ks.getKeys().size());
 	}
 	
 	@Test
@@ -63,16 +63,16 @@ public class KeyStorageTest {
 		ks.add(f1);
 		ks.add(f2);
 		ks.add(f3);
-		assertEquals(3,ks.size());
+		assertEquals(3,ks.getKeys().size());
 		
 		ks.delete(f1);
-		assertEquals(2,ks.size());
+		assertEquals(2,ks.getKeys().size());
 		assertNull   (ks.find(f1));
 		assertNotNull(ks.find(f2));
 		assertNotNull(ks.find(f3));
 		
 		ks.delete(f3);
-		assertEquals(1,ks.size());
+		assertEquals(1,ks.getKeys().size());
 		assertNull   (ks.find(f1));
 		assertNotNull(ks.find(f2));
 		assertNull   (ks.find(f3));
@@ -84,6 +84,7 @@ public class KeyStorageTest {
 		ks.add(f3);
 		
 		ArrayList<Field> keys = ks.getKeys();
+		assertEquals(3,keys.size());
 		assertTrue (keys.contains(f1));
 		assertFalse(keys.contains(f2));
 		assertTrue (keys.contains(f3));
