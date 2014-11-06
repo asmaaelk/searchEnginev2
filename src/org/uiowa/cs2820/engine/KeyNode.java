53bede36a901740522eff1cb8007471e0dd785d2
MidnightPolaris <MidnightPolaris@users.noreply.github.com>
2014-11-06 14:50:08 -0600

    Work on KeyNode class

--- src/org/uiowa/cs2820/engine/KeyNode.java
+++ src/org/uiowa/cs2820/engine/KeyNode.java
@@ -1,5 +1,26 @@
 package org.uiowa.cs2820.engine;
 
 public class KeyNode {
-
+	protected Field key;
+	public int prev;
+	public int next;
+	public int value;
+	
+	KeyNode(Field f){
+		this(f,-1,-1,-1);
+	}
+	KeyNode(Field f, int p, int n, int v){
+		key = f;
+		prev = p;
+		next = n;
+		value = v;
+	}
+	
+	public boolean match(Field f){
+		return key.getFieldName().equals(f.getFieldName()) && key.getFieldValue().equals(f.getFieldValue());
+	}
+	
+	public Field getKey(){
+		return key;
+	}
 }
