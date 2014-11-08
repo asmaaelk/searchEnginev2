90f5edff2b080946434e3a4f45640dc93a8723b6
andrewhoch <andrew-hoch@uiowa.edu>
2014-11-08 16:48:54 -0600

    Update Allocate.java

--- src/org/uiowa/cs2820/engine/Allocate.java
+++ src/org/uiowa/cs2820/engine/Allocate.java
@@ -1,11 +1,24 @@
 package org.uiowa.cs2820.engine;
+
+import java.util.BitSet;
 //andrew hoch
+//0 free
+//1 used
 
 public class Allocate{
+	private BitSet theBitSet = new BitSet();
+	
 	public int allocate(){
-		return 0;
+		int firstzero =  theBitSet.nextClearBit(0);
+		return firstzero;
 	}
-	public int free(){
-		return 0;
+	
+	public void allocate(int area){
+		theBitSet.set(area);
+		
+	}
+	public void free(int area){
+		theBitSet.clear(area);
+		return;
 	}
 }
