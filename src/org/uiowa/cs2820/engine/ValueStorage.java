3e921a6221f427ff8c2f715d41adbfc8efb318c1
Asmaa Elkeurti <asmaa.elkeurti@gmail.com>
2014-11-08 16:15:31 -0600

    added method skeletons

---
+++ src/org/uiowa/cs2820/engine/ValueStorage.java
@@ -0,0 +1,22 @@
+package org.uiowa.cs2820.engine;
+
+public class ValueStorage {
+	
+	public void delete() 
+	{
+		
+	}
+	
+	public void read(String identifier) 
+	{
+		
+	}
+	
+	public void write(String identifier) 
+	{
+		
+	}
+	
+	
+
+}
