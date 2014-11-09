15081fe58b6e9368f13d4dce6a2f36148281242d
Asmaa Elkeurti <asmaa.elkeurti@gmail.com>
2014-11-09 17:54:29 -0600

    Added required DiskSpace function read

--- src/org/uiowa/cs2820/engine/DiskSpace.java
+++ src/org/uiowa/cs2820/engine/DiskSpace.java
@@ -4,13 +4,18 @@
 package org.uiowa.cs2820.engine;
 
 import java.io.IOException;
+import java.io.File;
 import java.io.RandomAccessFile;
 
-public class RandomAccessFileEx {
 /*
 This is a working IMPLEMENTATION from link provided at top. 
-To implement, change the filepath as needed.
+To implement, change the filepath as needed.*/
 
+public class RandomAccessFileEx {
+	private static File file;
+	private static String filePath;
+
+/*
 static final String FILEPATH = "C:/Users/USER_NAME/just_a_path/input.txt";
 	public static void main(String[] args) {
 		try {
@@ -24,17 +29,27 @@ static final String FILEPATH = "C:/Users/USER_NAME/just_a_path/input.txt";
 
 	}*/
 
-	private static byte[] readFromFile(String filePath, int position, int size)
-			throws IOException {
-
-		RandomAccessFile file = new RandomAccessFile(filePath, "r");
+	/*private static byte[] readFromFile(String filePath, int position, int size) throws IOException {
+	private static byte[] read(long area) throws IOException{
+		
+		RandomAccessFile file = new RandomAccessFile(filePath, "rw");
 		file.seek(position);
 		byte[] bytes = new byte[size];
 		file.read(bytes);
 		file.close();
 		return bytes;
 
-	}//End read
+	}//End read*/
+	private static byte[] read(long area) throws IOException{
+		
+		RandomAccessFile randFile = new RandomAccessFile(filePath, "rw");
+		byte[] bytes = new byte[DiskSpace.getFileSize()];
+		randFile.seek(area);
+		randFile.readFully(bytes);
+		randFile.close();
+		return bytes;
+		
+	}
 
 	private static void writeToFile(String filePath, String data, int position)
 			throws IOException {
@@ -44,5 +59,11 @@ static final String FILEPATH = "C:/Users/USER_NAME/just_a_path/input.txt";
 		file.write(data.getBytes());
 		file.close();
 
-	}//End write
+	}
+	//End write
+	
+	public static int getFileSize() {
+		int fileSize = (int) file.length();
+		return fileSize;
+	}
 }//End DiskSpace()
