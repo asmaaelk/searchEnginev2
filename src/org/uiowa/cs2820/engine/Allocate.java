b41d12918eec02e894c98b225d8cdd1592d43883
andrewhoch <andrew-hoch@uiowa.edu>
2014-11-13 06:09:07 -0600

    updates to checkpoint and allocate and their tests

--- src/org/uiowa/cs2820/engine/Allocate.java
+++ src/org/uiowa/cs2820/engine/Allocate.java
@@ -25,10 +25,19 @@ public class Allocate{
 		restoreSave();
 	}
 	
+	//makes changes to the file from allocate and free	
 	public static void restoreSave() throws IOException{
 		Object fileObject = Checkpoint.restore();
 		BitSet fileBitSet = (BitSet) fileObject;		
 		fileBitSet = theBitSet;		
 		Checkpoint.save(fileBitSet);		
+	}
+	
+	//methods used in testing
+	public static boolean getValue(int arg0){
+		return theBitSet.get(arg0);
+	}	
+	public static void setBit(int arg0, boolean value){
+		theBitSet.set(arg0, value);
 	}
 }
--- src/org/uiowa/cs2820/engine/Checkpoint.java
+++ src/org/uiowa/cs2820/engine/Checkpoint.java
@@ -1,25 +1,31 @@
 package org.uiowa.cs2820.engine;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
+
 
 
 
 //andrew hoch
 
-public class Checkpoint {
-
+public class Checkpoint {	
+	static File file = new File("checkpoint_file");
     public static void save(Object obj) throws IOException{
-    	byte[] buffer = Field.convert(obj);
-    	Files.write(Paths.get("checkpoint_file"), buffer);
+    	byte[] buffer = Field.convert(obj);    	
+    	FileOutputStream f = new FileOutputStream(file);
+    	f.write(buffer);
+    	f.close();
     	
     	
     }
-    public static Object restore() throws IOException{
-    	Path path = Paths.get("checkpoint_file");
-    	byte [] buffer = Files.readAllBytes(path);
+    public static Object restore() throws IOException{    	
+    	FileInputStream f = new FileInputStream(file);
+    	byte[] buffer = new byte[(int) file.length()];
+    	f.read(buffer);
+    	f.close();
     	Object theobject = Field.revert(buffer);
     	return theobject;
     }
