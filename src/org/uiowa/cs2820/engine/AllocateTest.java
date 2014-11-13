7baee36b08afe2779cd3b31d4cc713e6571879ee
andrewhoch <andrew-hoch@uiowa.edu>
2014-11-12 22:19:13 -0600

    adding test cases for allocate and checkpoint

---
+++ src/org/uiowa/cs2820/engine/AllocateTest.java
@@ -0,0 +1,24 @@
+package org.uiowa.cs2820.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AllocateTest {
+
+	@Test
+	public void testAllocate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllocateInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFree() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/org/uiowa/cs2820/engine/checkpointTest.java
@@ -0,0 +1,19 @@
+package org.uiowa.cs2820.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class checkpointTest {
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRestore() {
+		fail("Not yet implemented");
+	}
+
+}
