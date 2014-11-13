6ee05c89e14b10397502a6c950ea3bab7bd4cfe5
andrewhoch <andrew-hoch@uiowa.edu>
2014-11-12 22:15:28 -0600

    Update checkpoint.java

--- src/org/uiowa/cs2820/engine/checkpoint.java
+++ src/org/uiowa/cs2820/engine/checkpoint.java
@@ -1,6 +1,5 @@
 package org.uiowa.cs2820.engine;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,24 +10,17 @@ import java.nio.file.Paths;
 //andrew hoch
 
 public class checkpoint {
-    //public static byte[] save(Object obj)  {    	
-        //return Field.convert(obj);
-    //}
 
-    //public static Object restore(byte[] bytes)  {
-        //return Field.revert(bytes);
-    //}
     public static void save(Object obj) throws IOException{
-    	byte[] theArray = Field.convert(obj);
-    	Files.write(Paths.get("target-file"), theArray);
+    	byte[] buffer = Field.convert(obj);
+    	Files.write(Paths.get("target-file"), buffer);
     	
     	
     }
     public static Object restore(String filename) throws IOException{
     	Path path = Paths.get(filename);
-    	byte [] theArray = Files.readAllBytes(path);
-    	Object theobject = Field.revert(theArray);
+    	byte [] buffer = Files.readAllBytes(path);
+    	Object theobject = Field.revert(buffer);
     	return theobject;
     }
-
 }
