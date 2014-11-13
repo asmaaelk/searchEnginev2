f0351316f4471c53928f8d4f88efc4822cb6ffe8
andrewhoch <andrew-hoch@uiowa.edu>
2014-11-13 07:22:43 -0600

    updates to checkpoint

--- src/org/uiowa/cs2820/engine/Checkpoint.java
+++ src/org/uiowa/cs2820/engine/Checkpoint.java
@@ -2,6 +2,7 @@ package org.uiowa.cs2820.engine;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -12,16 +13,17 @@ import java.io.IOException;
 //andrew hoch
 
 public class Checkpoint {	
-	static File file = new File("checkpoint_file");
-    public static void save(Object obj) throws IOException{
+	static File file;
+    public static void save(Object obj) throws IOException, FileNotFoundException{
+    	if (file.exists() == false){file = new File("checkpoint_file");}
     	byte[] buffer = Field.convert(obj);    	
     	FileOutputStream f = new FileOutputStream(file);
     	f.write(buffer);
-    	f.close();
-    	
-    	
+		f.close();    	
     }
-    public static Object restore() throws IOException{    	
+    
+    public static Object restore() throws IOException, FileNotFoundException{    	
+    	if (file.exists() == false) {file = new File("checkpoint_file");}    	
     	FileInputStream f = new FileInputStream(file);
     	byte[] buffer = new byte[(int) file.length()];
     	f.read(buffer);
@@ -29,4 +31,5 @@ public class Checkpoint {
     	Object theobject = Field.revert(buffer);
     	return theobject;
     }
+
 }
