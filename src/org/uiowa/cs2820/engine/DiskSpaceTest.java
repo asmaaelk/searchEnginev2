826a5d2a5bcadf7a59629f5495254328f198fc53
Twoody <tanner.l.woody@gmail.com>
2014-11-14 23:20:48 -0600

    Update DiskSpaceTest.java
    DiskSpace test is done

--- src/org/uiowa/cs2820/engine/DiskSpaceTest.java
+++ src/org/uiowa/cs2820/engine/DiskSpaceTest.java
@@ -1,13 +1,10 @@
 package org.uiowa.cs2820.engine;
 import static org.junit.Assert.*;
-
 import java.io.IOException;
-
 import org.junit.Test;
 
 public class DiskSpaceTest {
  
-	
 	;
 	
 	@Test
@@ -15,11 +12,9 @@ public class DiskSpaceTest {
 		String testee = "Object Oriented rox";
 		DiskSpace disk = new DiskSpace("DiskSpace.txt");
 		byte[] object = Utility.convert(testee);
-		disk.writeByte(object,0);
-		byte[] read = disk.readArea(0);
-		String data =  (String) Utility.revert(read);
-		assertEquals(A, data);
-		
-		
+		DiskSpace.write(object,0);
+		byte[] reader = DiskSpace.read(0);
+		String data =  (String) Utility.revert(reader);
+		assertEquals(testee, data);
 	}
 }
